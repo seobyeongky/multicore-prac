b8e37ddf701b67ed2e68958bf3e80d5a37f03dd1
Byeongki Seo <9ttttttttt@hanyang.ac.kr>
2016-09-09 14:19:08 +0900

    lab1 pthread practice

---
+++ prac_pthread/prac_pthread.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define NUM_THREAD	10
+#define NUM_INCREASE	1000000
+
+int cnt_global = 0;
+
+void *ThreadFunc(void *arg) {
+	long cnt_local = 0;
+
+	for (int i = 0; i < NUM_INCREASE; i++) {
+		cnt_global++;
+		cnt_local++;
+	}
+
+	return (void*)cnt_local;
+}
+
+int main(void) {
+	pthread_t threads[NUM_THREAD];
+
+	// create threads
+	for (int i = 0; i < NUM_THREAD; i++) {
+		if (pthread_create(&threads[i], 0, ThreadFunc, NULL) < 0) {
+			printf("pthread_create error!\n");
+			return 0;
+		}
+	}
+
+	// wait threads end
+	long ret;
+	for (int i = 0; i < NUM_THREAD; i++) {
+		pthread_join(threads[i], (void**)&ret);
+		printf("thread %d, local count: %d\n", threads[i], ret);
+	}
+
+	printf("global count: %d\n", cnt_global);
+	
+	return 0;
+}
