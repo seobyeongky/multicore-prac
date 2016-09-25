bf2874c04560979e77e11421902d7e0fe2808715
Byeongki Seo <9ttttttttt@hanyang.ac.kr>
2016-09-25 23:28:18 +0900

    restrict the number of working threads by number of cores
    Former-commit-id: 3f6c90544d738b16b4d2ef58b869040fc2c675c7

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -11,12 +11,13 @@
 using namespace std;
 
 Context g_context;
+int g_house_index;
+pthread_mutex_t g_house_index_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 class Worker {
 public:
-    Worker(int house, int num_allowed_threads)
+    Worker(int num_allowed_threads)
         : thread_()
-        , house_(house)
         , result_()
         , num_allowed_threads_(num_allowed_threads) {
     }
@@ -33,15 +34,26 @@ public:
 
 private:
     pthread_t thread_;
-    int house_;
     BFSResult result_;
     int num_allowed_threads_;
 
     void *BFSThread() {
-        if (num_allowed_threads_ == 1) {
-            result_.Update(BFSSingleThread(house_));
-        } else {
-            result_.Update(BFSMultiThread(house_, num_allowed_threads_));
+        while (g_house_index < (int)g_context.houses.size() - 1) {
+
+            pthread_mutex_lock(&g_house_index_mutex);
+            int house_index = g_house_index++;
+            pthread_mutex_unlock(&g_house_index_mutex);
+
+            if (house_index >= (int)g_context.houses.size() - 1) {
+                break;
+            }
+
+            int house = g_context.houses[house_index];
+            if (num_allowed_threads_ == 1) {
+                result_.Update(BFSSingleThread(house));
+            } else {
+                result_.Update(BFSMultiThread(house, num_allowed_threads_));
+            }
         }
 
         return &result_;
@@ -71,8 +83,8 @@ int main() {
     size_t num_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
     vector<Worker*> workers;
-    for (unsigned int i = 0; i < g_context.houses.size(); i++) {
-        workers.push_back(new Worker(g_context.houses[i], 1));
+    for (unsigned int i = 0; i < num_cores; i++) {
+        workers.push_back(new Worker(1));
     }
     
     for (Worker * worker : workers) {
