652f4cab927fe754051638c46d80ebd59a32e4dd
Byeongki Seo <9ttttttttt@hanyang.ac.kr>
2016-10-25 17:52:32 +0900

    working
    Former-commit-id: b121dd650af819fb331fa5c5ee6a967741d9e707

--- Project2/src/mvcc.cpp
+++ Project2/src/mvcc.cpp
@@ -6,6 +6,13 @@
 #include <list>
 #include <vector>
 #include <iostream>
+#include <limits.h>
+#include <assert.h>
+#include <string.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "node.h"
 
 using namespace std;
 
@@ -22,7 +29,7 @@ struct Data {
 
 struct ThreadContext {
     int id;
-    list<Data> data_list;
+    Node<Data> * data_head;
     int num_updates;
 };
 
@@ -39,23 +46,101 @@ int g_k = 0;
 int g_num_threads;
 int g_dura;
 bool g_over = false;
+bool g_verbose = false;
 pthread_mutex_t g_mutex = PTHREAD_MUTEX_INITIALIZER;
-list<ActiveThread> g_active_thread_list;
+Node<ActiveThread> * g_active_thread_head;
 vector<ThreadContext> g_contexts;
 const int A_PLUS_B = 100;
+const int VERBOSE_OUTPUT_INTERVAL = 100;
 
 template <typename T>
 inline T Square(T x) {
     return x * x;
 }
 
+void GC() {
+    int ref_min_version = INT_MAX;
+
+    pthread_mutex_lock(&g_mutex);
+    //printf("GC AC :");
+    for (Node<ActiveThread> *node = g_active_thread_head;
+            node != nullptr; node = node->next) {
+        if (node->next == nullptr) {
+            ref_min_version = node->data.version;
+            break;
+        }
+    }
+    //printf("\n");
+    pthread_mutex_unlock(&g_mutex);
+
+    for (ThreadContext & cx : g_contexts) { 
+        if (cx.data_head && cx.data_head->data.version < ref_min_version) {
+            ref_min_version = cx.data_head->data.version;
+        }
+    }
+
+    //printf("GC under %d\n", ref_min_version);
+
+    for (int i = 0; i < g_num_threads; i++) {
+        ThreadContext & cx = g_contexts[i];
+        for (Node<Data> * node = cx.data_head; node != nullptr; node = node->next) {
+            if (node->data.version < ref_min_version) {
+                node = node->next;
+                if (node != nullptr) {
+                    node = node->next;
+                }
+                Node<Data> * prev_node = node;
+                if (node != nullptr) {
+                    node = node->next;
+                }
+                if (node != nullptr) {
+                    //printf("[%d] kill under : %d\n", i, node->data.version);
+                }
+                while (node != nullptr) {
+                    node = NodeErase(prev_node, node);
+                }
+                break;
+            }
+        }
+    }
+}
+
+void * ScheduleGC(void *arg) {
+    while (!g_over) {
+        GC();
+    }
+
+    return nullptr;
+}
+
 void *UpdateAB(void *arg) {
     ThreadContext & self = *((ThreadContext *)arg);
+    FILE * out;
+
+    if (g_verbose) {
+        char filename[256];
+        sprintf(filename, "verbose_thread_%d.txt", self.id);
+        out = fopen(filename, "w");
+    }
+
+    timespec prev_clock;
+    int acc_time = 0;
+    int verbose_output_count = 0;
+    if (g_verbose) {
+        clock_gettime(CLOCK_MONOTONIC_RAW, &prev_clock);
+    }
+
     while (!g_over) {
         pthread_mutex_lock(&g_mutex);
         int my_version = g_k++;
-        auto it = g_active_thread_list.insert(g_active_thread_list.end(), ActiveThread());
-        list<ActiveThread> RV(g_active_thread_list);
+        int my_recent_version = self.data_head->data.version;
+        Node<ActiveThread> * my_node = NodePushFront(&g_active_thread_head, ActiveThread(self.id
+                    , my_recent_version));
+        Node<ActiveThread> *RV = nullptr;
+        for (Node<ActiveThread> *node = g_active_thread_head;
+                node != nullptr; node = node->next) {
+            NodePushFront(&RV, node->data);
+        }
         pthread_mutex_unlock(&g_mutex);
 
         int i = rand() % g_contexts.size();
@@ -63,7 +148,9 @@ void *UpdateAB(void *arg) {
         int target_version;
         Data data_i;
         
-        for (ActiveThread active_thread : RV) {
+        for (Node<ActiveThread> * ac_node = RV;
+                ac_node != nullptr; ac_node = ac_node->next) {
+            ActiveThread & active_thread = ac_node->data;
             if (active_thread.id == i) {
                 contains = true;
                 target_version = active_thread.version;
@@ -73,23 +160,65 @@ void *UpdateAB(void *arg) {
 
         ThreadContext & thread_i = g_contexts[i];
         if (contains) {
-            for (Data data : thread_i.data_list) {
-                if (data.version == target_version) {
-                    data_i = data;
+            for (Node<Data> *data_node = thread_i.data_head;
+                    data_node != nullptr; data_node = data_node->next) {
+                //printf("[%d] ref %d : %d %d\n", self.id, i, target_version, data_node->data.version);
+                assert(data_node->data.version >= target_version);
+                if (data_node->data.version == target_version) {
+                    data_i = data_node->data;
+                    break;
+                }
+            }
+        } else {
+            for (Node<Data> *data_node = thread_i.data_head;
+                    data_node != nullptr; data_node = data_node->next) {
+                //printf("[%d] ref2 %d : %d %d\n", self.id, i, my_recent_version, data_node->data.version);
+                if (data_node->data.version < my_recent_version) {
+                    data_i = data_node->data;
                     break;
                 }
             }
         }
 
-        Data data_x(self.data_list.front());
+        Data data_x(self.data_head->data);
 
-        self.data_list.push_front(Data(my_version, data_x.A + data_i.A % 100, data_x.B - data_i.A % 100));
+        NodePushFront(&self.data_head
+                , Data(my_version, data_x.A + data_i.A % 100, data_x.B - data_i.A % 100));
 
         pthread_mutex_lock(&g_mutex);
-        g_active_thread_list.erase(it);
+        NodeErase(&g_active_thread_head, my_node);
         pthread_mutex_unlock(&g_mutex);
 
+        if (g_verbose) {
+            timespec now_clock;
+            clock_gettime(CLOCK_MONOTONIC_RAW, &now_clock);
+
+            int dt = (now_clock.tv_sec * 1000 + now_clock.tv_nsec / 1000000
+                    - prev_clock.tv_sec * 1000 - prev_clock.tv_nsec / 1000000);
+            prev_clock = now_clock;
+            acc_time += dt;
+            if (acc_time >= VERBOSE_OUTPUT_INTERVAL) {
+                acc_time -= VERBOSE_OUTPUT_INTERVAL;
+                verbose_output_count++;
+
+                fprintf(out, "read-view (at %.1fsec)\n", verbose_output_count * VERBOSE_OUTPUT_INTERVAL / 1000.f);
+                for (Node<ActiveThread> *node = RV;
+                        node != nullptr; node = node->next) {
+                    fprintf(out, "%d ", node->data.version);
+                }
+                fprintf(out, "\n");
+                fprintf(out, "%d\n", data_i.version);
+                fprintf(out, "\n");
+            }
+        }
+
+        DestroyNodes(&RV);
+
         self.num_updates++;
+    }
+
+    if (g_verbose) {
+        fclose(out);
     }
 
     return nullptr;
@@ -99,7 +228,7 @@ void PrintSum() {
     int sum = 0;
     for (int i = 0; i < g_num_threads; i++) {
         ThreadContext * cx = &g_contexts[i];
-        Data & data = cx->data_list.front();
+        Data & data = cx->data_head->data;
         sum += data.A;
         sum += data.B;
     }
@@ -117,11 +246,11 @@ void PrintTotalThroughput() {
 }
 
 void PrintFairness() {
-    int sum = 0;
-    int sq_sum = 0;
+    long long int sum = 0;
+    long long int sq_sum = 0;
     for (ThreadContext & cx : g_contexts) {
         sum += cx.num_updates;
-        sq_sum += Square(cx.num_updates);
+        sq_sum += Square((long long int)cx.num_updates);
     }
 
     double fairness = (double)Square(sum) / (g_num_threads * sq_sum);
@@ -137,6 +266,12 @@ int main(int argc, char * argv []) {
 
     g_num_threads = atoi(argv[1]);
     g_dura = atoi(argv[2]);
+    if (argc >= 4) {
+        if (!strcmp(argv[3], "verbose")) {
+            g_verbose = true;
+        }
+    }
+
     vector<pthread_t> threads(g_num_threads);
     g_contexts.resize(g_num_threads);
 
@@ -147,7 +282,7 @@ int main(int argc, char * argv []) {
         data.A = rand() % A_PLUS_B;
         data.B = A_PLUS_B - data.A;
         data.version = g_k++;
-        cx->data_list.push_back(data);
+        NodePushFront(&cx->data_head, data);
         cx->num_updates = 0;
     }
 
@@ -156,8 +291,13 @@ int main(int argc, char * argv []) {
         pthread_create(&threads[i], 0, UpdateAB, (void *)&g_contexts[i]);
     }
 
+    pthread_t gc_thread;
+    pthread_create(&gc_thread, 0, ScheduleGC, 0);
+
     sleep(g_dura);
     g_over = true;
+
+    pthread_join(gc_thread, 0);
 
     for (int i = 0; i < g_num_threads; i++) {
         pthread_join(threads[i], 0);
--- Project2/src/node.h
+++ Project2/src/node.h
@@ -2,9 +2,62 @@
 #define __PROJECT2_SLIST_H__
 
 template <typename T>
-class Node {
+struct Node {
 public:
-        
+    Node * next;       
+    T data;
+
+    Node() {}
+
+    Node(Node * next, const T & data)
+        : next(next), data(data) {}
+};
+
+template <typename T>
+Node<T> * NodePushFront(Node<T> ** head, const T & data) {
+    Node<T> * new_node = new Node<T>(*head, data);
+    *head = new_node;
+    return new_node;
 }
+
+template <typename T>
+Node<T> * NodeErase(Node<T> ** head, Node<T> * node) {
+    Node<T> * prev_node = nullptr;
+    for (Node<T> * it = *head; it != nullptr; it = it->next) {
+        if (it == node) {
+            break;
+        }
+        prev_node = it;
+    }
+
+    if (prev_node == nullptr) {
+        // That means node is head
+        *head = nullptr;
+    }
+    
+    return NodeErase(prev_node, node);
+}
+
+template <typename T>
+Node<T> * NodeErase(Node<T> * prev_node, Node<T> * node) {
+    if (prev_node != nullptr) {
+        prev_node->next = node->next;
+    }
+
+    delete node;
+
+    return prev_node ? prev_node->next : nullptr;
+}
+
+template <typename T>
+void DestroyNodes(Node<T> ** head) {
+    for (Node<T> * it = *head; it != nullptr;) {
+        Node<T> *next_it = it->next;
+        delete it;
+        it = next_it;
+    }
+    *head = nullptr;
+}
+
 
 #endif /* __PROJECT2_SLIST_H__ */
