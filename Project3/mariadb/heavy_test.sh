3170f5aed40aa443c47554f11b9a0359a1ea8364
Byeongki Seo <9ttttttttt@hanyang.ac.kr>
2016-12-23 01:31:32 +0900

    heavy test script modified
    Former-commit-id: 876cc32a7f0e60eed080d5feff8c58507e8ea8a9

--- Project3/mariadb/heavy_test.sh
+++ Project3/mariadb/heavy_test.sh
@@ -1,4 +1,4 @@
-run/bin/mysqlslap --create-schema=test --query="INSERT INTO food VALUES (0, 'Fried Chicken', 6)" --concurrency=20 --number-of-queries=10000 --iterations=5
+run/bin/mysqlslap --create-schema=test --query="INSERT INTO food VALUES (0, 'Fried Chicken', 6)" --concurrency=30 --number-of-queries=20000 --iterations=2
 
 run/bin/mysql -e "DELETE FROM food;" test
 
